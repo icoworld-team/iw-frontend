821b7aea67d542cf29ccbad4972b0839600d4e6b
Vladislav <vladnev21@gmail.com>
2018-08-10 01:39:04 +0500

    qwe

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,8 @@
-// // Create a reusable render method that we can call more than once
-// let render = () => {
-//     // Dynamically import our main App component, and render it
-//     const MainApp = require('./MainApp').default;
-//     ReactDOM.render(
-//         <MainApp/>,
-//         rootEl
-//     );
-// };
+import React from 'react'
+import {render} from "react-dom";
+// import { Provider } from 'react-redux';
+// import { ConnectedRouter } from 'react-router-redux';
+import FullFeatured from './App'
 
-// if (module.hot) {
-//     module.hot.accept('./MainApp', () => {
-//         const NextApp = require('./MainApp').default;
-//         render(
-//             <NextApp/>,
-//             rootEl
-//         );
-//     });
-// }
 
-// render();
-
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-import { Hello } from "./components/Hello";
-
-const rootEl = document.getElementById('app-site');
-
-ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React!!!" />,
-    rootEl
-);
+render( <FullFeatured />, document.getElementById('root'));
