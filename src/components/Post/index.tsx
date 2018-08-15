4eecb6f7507fe86fe6f70ae111a3a22a47839cea
Artur Ovcharenko <stevvy1122@gmail.com>
2018-08-15 15:56:11 +0300

    design updates

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,20 +1,41 @@
 import React, {Component} from 'react'
+import { createStyles, withStyles } from '@material-ui/core/styles';
 import Favorite from '@material-ui/icons/Favorite'
-//import FavoriteBorder from '@material-ui/icons/FavoriteBorder'
 import Comment from '@material-ui/icons/Comment'
 import Repeat from '@material-ui/icons/Repeat'
 import IconButton from '@material-ui/core/IconButton'
-import Divider from '@material-ui/core/Divider'
+import Divider from '@material-ui/core/Divider';
+import FormControl from '@material-ui/core/FormControl';
+import InputAdornment from '@material-ui/core/InputAdornment';
 import './style.css'
 import Fade from "@material-ui/core/Fade";
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Input from '@material-ui/core/Input';
+import Send from '@material-ui/icons/Send';
 
+const styles = () => createStyles({
+    textField: {
+      width: '100%',
+    },
+    show: {
+      height: '32px',
+    },
+    hidden: {
+        height: 0,
+    },
+    container: {
+        overflow: 'hidden',
+        transition: '.3s'
+    }
+  });
 
-export default class Post extends Component<any> {
+class Post extends Component<any> {
     state = {
-        anchorEl: undefined
+        anchorEl: undefined,
+        comment: '',
+        showInput: false
     };
 
     handleClick = (event:any)=> {
@@ -25,15 +46,33 @@ export default class Post extends Component<any> {
         this.setState({ anchorEl: null});
     };
 
+    handleChange = (prop: any) => (event: any) => {
+        this.setState({ [prop]: event.target.value });
+    };
+
+    handleClickShowInput = () => {
+        this.setState(state => ({ showInput: !this.state.showInput }));
+    }
+
+    handleSendComment = () => {
+        alert(`Введённое сообщение: ${this.state.comment}`);
+    }
+
+    handleKeyDownSendComment = (event: any) => {
+        if(event.keyCode == 13) {
+            this.handleSendComment();
+        }
+    }
 
     render() {
-        const {post} = this.props;
+        const { post } = this.props;
+        const { classes } = this.props;
         return (
             <div className="post">
                 <div className="post-header">
                     <div className="post-left-header">
                         <div>
-                            <img className="post-avatar" src="profile.jpeg" width="60px"/>
+                            <img className="post-avatar" src="profile.jpeg" width="50px"/>
                         </div>
                         <div className="post-header-text">
                             <div className="inline-flex">
@@ -61,11 +100,35 @@ export default class Post extends Component<any> {
                         <Favorite color="primary"/>
                     </IconButton>
                     <IconButton>
-                        <Comment color="primary"/>
+                        <Comment
+                            color="primary"
+                            onClick={this.handleClickShowInput}
+                        />
                     </IconButton>
                     <IconButton>
                         <Repeat color="primary"/>
                     </IconButton>
+                </div>
+                <div className={`${classes.container} ${this.state.showInput ? classes.show : classes.hidden}`}>
+                    <FormControl className={classes.textField}>
+                        <Input
+                            id="comment"
+                            type='text'
+                            value={this.state.comment}
+                            onChange={this.handleChange('comment')}
+                            onKeyDown={this.handleKeyDownSendComment}
+                            endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="Toggle comment input"
+                                    onClick={this.handleSendComment}
+                                >
+                                <Send />
+                                </IconButton>
+                            </InputAdornment>
+                            }
+                        />
+                    </FormControl>
                 </div>
                 <Divider/>
             </div>
@@ -74,3 +137,4 @@ export default class Post extends Component<any> {
 
 }
 
+export default withStyles(styles)(Post);
--- src/components/PostInput/index.tsx
+++ src/components/PostInput/index.tsx
@@ -1,4 +1,5 @@
 import React,{Component} from 'react'
+import { createStyles, withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton';
@@ -6,20 +7,33 @@ import InsertPhoto from '@material-ui/icons/InsertPhoto'
 import InsertDriveFile from '@material-ui/icons/InsertDriveFile'
 import './style.css'
 
-class PostInput extends Component {
+const styles = () => createStyles({
+    postInput: {
+        margin: '20px 0',
+    },
+    attachment: {
+        display: 'none',
+    },
+    textInput: {
+        backgroundColor: '#ececec',
+    }
+});
+
+class PostInput extends Component<any> {
 
     render() {
+        const { classes } = this.props;
         return (
-            <div className="post-input">
-                <TextField fullWidth multiline rows="6"/>
+            <div className={classes.postInput}>
+                <TextField className={classes.textInput} fullWidth multiline rows="6"/>
                 <Button variant="raised" color="primary">Post</Button>
-                <input className="attachment" id="image-file" type="file" accept="image/*"/>
+                <input className={classes.attachment} id="image-file" type="file" accept="image/*"/>
                 <label htmlFor="image-file">
                     <IconButton color="primary" component="span">
                         <InsertPhoto/>
                     </IconButton>
                 </label>
-                <input className="attachment" id="file" type="file"/>
+                <input className={classes.attachment} id="file" type="file"/>
                 <label htmlFor="file">
                     <IconButton color="primary" component="span">
                         <InsertDriveFile/>
@@ -30,4 +44,4 @@ class PostInput extends Component {
     }
 }
 
-export default PostInput
+export default withStyles(styles)(PostInput);
