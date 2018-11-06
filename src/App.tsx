658e9bd2332556cd812fc25ae8ea0295f26b60e8
Artur Ovcharenko <stevvy1122@gmail.com>
2018-11-06 22:13:04 +0300

    landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,21 @@
 import React from 'react'
 import { Switch } from "react-router";
-import { Route } from 'react-router-dom'
-import { Provider } from "react-redux";
-import { ConnectedRouter } from "react-router-redux";
-import {store, history} from "./store";
-// import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from 'react-apollo'
-import MainApp from './components/MainApp'
+import { Route, Router } from 'react-router-dom'
+import { history } from "./store";
 import LandingPage from './components/LandingPage'
 import OfferProtectionPage from './components/OfferProtectionPage'
 import MarketMonopolyPage from './components/MarketMonopolyPage'
 import PitchForInvestorsPage from './components/PitchForInvestorsPage'
-import SignInPage from './components/SignInPage'
-import SignUpPage from './components/SignUpPage'
-import Contacts from './components/ContactsPage'
-import PasswordRecovery from './components/PasswordRecovery'
-import PrivateRoute from './components/PrivateRoute'
-import {endpoint} from "./api";
-import { ApolloClient } from 'apollo-client'
-// import { HttpLink } from 'apollo-link-http'
-import { onError } from 'apollo-link-error'
-import { ApolloLink } from 'apollo-link'
-import { createUploadLink } from 'apollo-upload-client'
-import { InMemoryCache, defaultDataIdFromObject } from 'apollo-cache-inmemory';
-
-const cache = new InMemoryCache({
-    dataIdFromObject: object => {
-        switch (object.__typename) {
-            case 'PostEditResponse': return (object as any).postId;
-            case 'Post': return (object as any).postId;
-            default: return defaultDataIdFromObject(object);
-        }
-    }
-});
-
-const client = new ApolloClient({
-    link: ApolloLink.from([
-        onError(({graphQLErrors, networkError}) => {
-            if (graphQLErrors) {
-                graphQLErrors.forEach(error => {
-                    if((error.extensions as any).code === '401') {
-                        localStorage.removeItem("user");
-                        window.location.reload();
-                    }
-                    if((error.extensions as any).code === '405') {
-                        console.log(error);
-                    }
-                    else console.log(`[GraphQL error]: Message: ${error.message}, Location: ${error.locations}, Path: ${error.path}`)
-                })
-            }
-            // if (graphQLErrors)
-            //     graphQLErrors.map(({message, locations, path, extensions}) =>
-            //         console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path} ${extensions}`));
-            if (networkError) console.log(`[Network error]: ${networkError}`);
-        }),
-        // new HttpLink({
-        //     uri: `${endpoint}/graphql`
-        // }),
-        createUploadLink({
-            uri: `${endpoint}/graphql`,
-            credentials: 'include'
-        })
-    ]),
-    cache: cache
-});
-
-// const client = new ApolloClient({
-//     uri: "http://icoworld.projects.oktend.com:3000/graphql",
-//     cache
-// });
 
 export default function App() {
     return (
-        <Provider store={store}>
-            <ConnectedRouter history={history}>
-                <ApolloProvider client={client}>
-                    <Switch>
-                        <Route path="/landing" component={LandingPage}/>
-                        <Route path="/market-monopoly" component={MarketMonopolyPage}/>
-                        <Route path="/offer-protection" component={OfferProtectionPage}/>
-                        <Route path="/pitch" component={PitchForInvestorsPage}/>
-                        <Route path="/signin" component={SignInPage}/>
-                        <Route path="/signup" component={SignUpPage}/>
-                        <Route path="/contacts" component={Contacts}/>
-                        <Route path="/reset" component={PasswordRecovery}/>
-                        <PrivateRoute path="/" component={MainApp}/>
-                    </Switch>
-                </ApolloProvider>
-            </ConnectedRouter>
-        </Provider>
+        <Router history={history}>
+            <Switch>
+                <Route path="/" component={LandingPage}/>
+                <Route path="/market-monopoly" component={MarketMonopolyPage}/>
+                <Route path="/offer-protection" component={OfferProtectionPage}/>
+                <Route path="/pitch" component={PitchForInvestorsPage}/>
+            </Switch>
+        </Router>
     )
 }
--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,33 +1,10 @@
-export const endpoint = 'https://icoworld.network';
+export const endpoint = 'http://icoworld.projects.oktend.com:3000';
 
 export const handleErrors = (response:any) => {
     if(!response.ok){
         throw Error(response.statusText);
     }
     return response;
-};
-
-export const fetchPost = (url:string, data:object) => {
-    return fetch(url, {
-        method: 'POST',
-        mode: 'cors',
-        body: JSON.stringify(data),
-        credentials: "include",
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    });
-};
-
-export const fetchGet =  (url:string) => {
-    return fetch(url, {
-        method: 'GET',
-        mode: 'cors',
-        credentials: "include",
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    });
 };
 
 export const sendEmail = (addr:string, title:string, content:string) => {
@@ -41,6 +18,3 @@ export const sendEmail = (addr:string, title:string, content:string) => {
         }
     }).then(response => handleErrors(response));
 };
-
-import io from 'socket.io-client'
-export const socket = io(endpoint, { transports: ['websocket'], autoConnect: false });
